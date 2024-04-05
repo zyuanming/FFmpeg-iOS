365d7ea4d606e5483a08c9305d8aa5c9d1f35109
zhangyuanming <zhangyuanming@bytedance.com>
2024-04-05 14:36:31 +0800

    Creating release for v0.0.6-b20240405-143628

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -30,8 +30,16 @@ TAG=v0.0.6-`date +b%Y%m%d-%H%M%S`
 echo "Install build dependencies..."
 brew install autoconf automake libtool pkg-config curl git doxygen nasm bison wget gettext gh
 
-echo "Getting started with GitHub CLI, run:  gh auth login"
-gh auth login
+echo "Check if the user is logged in to GitHub CLI"
+if gh auth status 2>&1 | grep -q 'You are not logged in to any GitHub hosts'; then
+  echo "You are not logged in to GitHub CLI."
+  # Call gh auth login to start the login process
+  gh auth login
+else
+  echo "You are already logged in to GitHub CLI."
+fi
+
+gh repo set-default zyuanming/FFmpeg-iOS
 
 echo "Committing Changes..."
 git add -u
