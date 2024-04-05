37d8c7f151bb15db267c345e13d32e6b07c0cb94
zhangyuanming <zhangyuanming@bytedance.com>
2024-04-05 14:48:00 +0800

    change tag

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-# git clone -b tool --depth 1 https://github.com/zyuanming/FFmpeg-iOS tool
-# cd tool
+rm -rf tool
+git clone -b tool --depth 1 https://github.com/zyuanming/FFmpeg-iOS tool
+cd tool
 
-# swift run
+swift run
 
-TAG=v0.0.6-`date +b%Y%m%d-%H%M%S`
+# TAG=v0.0.6-`date +b%Y%m%d-%H%M%S`
+TAG=0.0.7
 
-# cp ../Package.swift .
+cp ../Package.swift .
 
-# for f in Frameworks/*.xcframework
-# do
-# 	f=`basename $f .xcframework`
-# 	echo $f...
-# 	rm Package.swift.in
-# 	mv Package.swift Package.swift.in
-# 	sed "s#/download/[^/]*/$f\.zip[^)]*#/download/$TAG/$f.zip\", checksum: \"`swift package compute-checksum Frameworks/$f.zip`\"#" Package.swift.in > Package.swift
-# done
+for f in Frameworks/*.xcframework
+do
+	f=`basename $f .xcframework`
+	echo $f...
+	rm Package.swift.in
+	mv Package.swift Package.swift.in
+	sed "s#/download/[^/]*/$f\.zip[^)]*#/download/$TAG/$f.zip\", checksum: \"`swift package compute-checksum Frameworks/$f.zip`\"#" Package.swift.in > Package.swift
+done
 
-# rm ../Package.swift
-# mv Package.swift ..
+rm ../Package.swift
+mv Package.swift ..
 
-# echo "::set-output name=tag::$TAG"
+echo "::set-output name=tag::$TAG"
 
 
 
-# cd ..
+cd ..
 
 echo "Install build dependencies..."
 brew install autoconf automake libtool pkg-config curl git doxygen nasm bison wget gettext gh
