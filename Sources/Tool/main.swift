831e5083b014e48aa5fe35921060cd0288aa08db
zhangyuanming <zhangyuanming@bytedance.com>
2024-05-02 02:26:44 +0800

    feat: enable av1

--- Sources/Tool/main.swift
+++ Sources/Tool/main.swift
@@ -250,6 +250,7 @@ extension Tool {
                         "--enable-demuxer=matroska",
                         "--enable-muxer=matroska",
                         "--enable-decoder=av1",
+                        "--enable-libdav1d",
                         "--arch=\(arch)",
                         "--cc=\(cc)",
                         "--as=\(`as`)",
